fd05bfb24de5b53dabf6f075f7cb060ea39e1df9
samiHEL <sami.hella.sio@gmail.com>
2022-10-25 16:03:09 +0200

    fastapi 2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from enum import Enum
-
+from typing import Union
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class Item(BaseModel):
+    name: str
 
 app = FastAPI()
 
@@ -31,3 +33,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.post("/items1/")
+async def create_item(item: Item):
+    return item
